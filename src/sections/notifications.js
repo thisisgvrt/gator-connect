bc3b40fca96430dddadf32a15d54069bb5697ab3
tanianemeth <tnemeth@pipeline.sbcc.edu>
2020-08-04 10:19:54 -0700

    notifications and reminders screen added

--- src/sections/notifications.js
+++ src/sections/notifications.js
@@ -1,9 +1,164 @@
 import React from 'react';
 
 /* Tania */
-function NotificationScreen(){
+function NotificationScreen() {
+
     return (
-        <p>Notifications</p>
+     
+            <div className = "border-solid border-8 border-gray-100 rounded-lg">
+                <div class="overflow-x-auto">
+                    <div class="flex bg-blue-200 p-4 rounded-lg ">
+                        <div class="mr-4">
+                        <div class="flex bg-blue-200 p-4 rounded-b-lg">
+                    <div class="mr-4">
+                        <div class="h-10 w-10 text-white bg-blue-600 rounded-full flex justify-center items-center">
+                            <i class="material-icons">!</i>
+                        </div>
+                    </div>
+                    <div class="flex justify-between w-full">
+                        <div class="text-blue-600">
+                            <p class="mb-2 font-bold">
+                                Notifications
+      </p>
+                        </div>
+                     
+                    </div>
+                </div>
+         </div></div>
+
+
+                    <div class="flex bg-blue-200 p-4 mt-1 rounded-lg">
+                     
+                        <div class="flex justify-between w-full">
+                            <div class="text-blue-600">
+                                <p class="mb-2 font-bold">
+                                    Pending Invitation to join SFSU CS Club Group
+      </p>
+                                <p class="text-xs">
+                                    Accept/Decline
+      </p>
+                            </div>
+                            <div class="text-sm text-gray-500">
+                                <p>x</p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="flex bg-blue-200 p-4 mt-1 rounded-lg">
+                     
+                        <div class="flex justify-between w-full">
+                            <div class="text-blue-600">
+                                <p class="mb-2 font-bold">
+                                    SFSU Student Government posted an update
+      </p>
+                                <p class="text-xs">
+                                    Click to view
+      </p>
+                            </div>
+                            <div class="text-sm text-gray-500">
+                                <p>x</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="flex bg-blue-200 p-4 mt-1 rounded-lg">
+            
+                    <div class="flex justify-between w-full">
+                        <div class="text-blue-600">
+                            <p class="mb-2 font-bold">
+                                Event Milk and Bookies date and time was changed by host
+      </p>
+                            <p class="text-xs">
+                                Now 9/3/2020 @ 5:30 PM
+      </p>
+                        </div>
+                        <div class="text-sm text-gray-500">
+                            <p>x</p>
+                        </div>
+                    </div>
+                </div>
+
+    
+                <div class="flex bg-orange-200 p-4 mt-2 rounded-lg">
+                <div class="flex bg-orange-200 p-4 ">
+                    <div class="mr-4">
+                        <div class="h-10 w-10 text-white bg-orange-600 rounded-full flex justify-center items-center">
+                            <i class="material-icons">?</i>
+                        </div>
+                    </div>
+                    <div class="flex justify-between w-full">
+                        <div class="text-orange-600">
+                            <p class="mb-2 font-bold">
+                                Reminders
+      </p>
+        
+                        </div>
+                        <div class="text-sm text-gray-500">
+                            
+                        </div>
+                    </div>
+                </div>
+
+      </div>
+
+                <div class="flex bg-orange-200 mt-1 rounded-lg p-4">
+              
+                    <div class="flex justify-between w-full">
+                        <div class="text-orange-600">
+                            <p class="mb-2 font-bold">
+                                Today: Resume Builder Workshop
+      </p>
+                            <p class="text-xs">
+                                J. Leonard Library @ 1:15 PM
+      </p>
+                        </div>
+                        <div class="text-sm text-gray-500">
+                            <span>x</span>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="flex bg-orange-200 mt-1 rounded-lg p-4">
+               
+                    <div class="flex justify-between w-full">
+                        <div class="text-orange-600">
+                            <p class="mb-2 font-bold">
+                                Today: Microbiology Study Group
+      </p>
+                            <p class="text-xs">
+                                Thornton Hall 220 @ 5:00 PM
+      </p>
+                        </div>
+                        <div class="text-sm text-gray-500">
+                            <span>x</span>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="flex bg-orange-200 mt-1 p-4 rounded-lg">
+                    <div class="mr-4">
+                        <div class="h-10 w-10 text-white bg-orange-600 rounded-full flex justify-center items-center">
+                            <i class="material-icons">?</i>
+                        </div>
+                    </div>
+                    <div class="flex justify-between w-full">
+                        <div class="text-orange-600">
+                            <p class="mb-2 font-bold">
+                                Today: Resume Builder Workshop
+      </p>
+                            <p class="text-xs">
+                                J. Leonard Library @ 1:15 PM
+      </p>
+                        </div>
+                        <div class="text-sm text-gray-500">
+                            <span>x</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+     
     )
 }
 
