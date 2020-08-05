1385ab1c3c2c4d5e2fc64276f624a6415d031776
tanianemeth <tnemeth@pipeline.sbcc.edu>
2020-08-04 18:26:51 -0700

    added button to expand notifications/reminders

--- src/sections/notifications.js
+++ src/sections/notifications.js
@@ -27,22 +27,6 @@ function NotificationScreen() {
          </div></div>
 
 
-                    <div class="flex bg-blue-200 p-4 mt-1 rounded-lg">
-                     
-                        <div class="flex justify-between w-full">
-                            <div class="text-blue-600">
-                                <p class="mb-2 font-bold">
-                                    Pending Invitation to join SFSU CS Club Group
-      </p>
-                                <p class="text-xs">
-                                    Accept/Decline
-      </p>
-                            </div>
-                            <div class="text-sm text-gray-500">
-                                <p>x</p>
-                            </div>
-                        </div>
-                    </div>
 
                     <div class="flex bg-blue-200 p-4 mt-1 rounded-lg">
                      
@@ -78,7 +62,58 @@ function NotificationScreen() {
                         </div>
                     </div>
                 </div>
+                <p>
 
+  <button class="bg-blue-500"  type="button" data-toggle="collapse" data-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample" class ="rounded-lg">
+    <div class = "border-solid border-8 border-blue-500 rounded-lg">
+      <div class ="text-blue-300">
+        Click to expand 
+    </div>
+    </div>
+  </button>
+</p>
+<div class="collapse" id="collapseExample">
+  <div class="card card-body">
+    
+  <div class="flex bg-blue-200 p-4 mt-1 rounded-lg">
+                     
+                     <div class="flex justify-between w-full">
+                         <div class="text-blue-600">
+                             <p class="mb-2 font-bold">
+                                 Pending Invitation to join SFSU CS Club Group
+   </p>
+                             <p class="text-xs">
+                                 Accept/Decline
+   </p>
+                         </div>
+                         <div class="text-sm text-gray-500">
+                             <p>x</p>
+                         </div>
+                     </div>
+                 </div>
+  </div>
+</div>
+              {/* <html lang = "en">
+                  <head>
+                      <meta charset = "utf-8">
+                          <link rel="stylesheet" href="style.css">
+
+                          </link>
+
+                      </meta>
+                  </head>
+                  <body>
+                      <div class="container">
+                          <button class="Accordion">Click to expand</button>
+                          <div class="accordion-content">
+                              <p>
+                                  "something here"
+                              </p>
+                          </div>
+                      </div>
+
+                  </body>
+              </html> */}
     
                 <div class="flex bg-orange-200 p-4 mt-2 rounded-lg">
                 <div class="flex bg-orange-200 p-4 ">
@@ -118,6 +153,7 @@ function NotificationScreen() {
                         </div>
                     </div>
                 </div>
+                
 
                 <div class="flex bg-orange-200 mt-1 rounded-lg p-4">
                
@@ -136,27 +172,42 @@ function NotificationScreen() {
                     </div>
                 </div>
 
-                <div class="flex bg-orange-200 mt-1 p-4 rounded-lg">
-                    <div class="mr-4">
-                        <div class="h-10 w-10 text-white bg-orange-600 rounded-full flex justify-center items-center">
-                            <i class="material-icons">?</i>
-                        </div>
-                    </div>
-                    <div class="flex justify-between w-full">
-                        <div class="text-orange-600">
-                            <p class="mb-2 font-bold">
-                                Today: Resume Builder Workshop
-      </p>
-                            <p class="text-xs">
-                                J. Leonard Library @ 1:15 PM
-      </p>
-                        </div>
-                        <div class="text-sm text-gray-500">
-                            <span>x</span>
-                        </div>
-                    </div>
-                </div>
+                
+                <p>
+
+<button class="bg-orange-500"  type="button" data-toggle="collapse" data-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample" class ="rounded-lg">
+  <div class = "border-solid border-8 border-orange-500 rounded-lg">
+    <div class ="text-orange-300">
+      Click to expand 
+  </div>
+  </div>
+</button>
+</p>
+<div class="collapse" id="collapseExample">
+<div class="card card-body">
+  
+<div class="flex bg-orange-200 p-4 mt-1 rounded-lg">
+                   
+                   <div class="flex justify-between w-full">
+                       <div class="text-orange-600">
+                           <p class="mb-2 font-bold">
+                               Pending Invitation to join SFSU CS Club Group
+ </p>
+                           <p class="text-xs">
+                               Accept/Decline
+ </p>
+                       </div>
+                       <div class="text-sm text-gray-500">
+                           <p>x</p>
+                       </div>
+                   </div>
+               </div>
+</div>
+</div>
+                
             </div>
+          
+           
 
      
     )
