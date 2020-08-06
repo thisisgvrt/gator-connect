5860703cd70d937cd0c1dedf1b97b53a09cad29b
tanianemeth <tnemeth@pipeline.sbcc.edu>
2020-08-05 21:52:10 -0700

    added key message feature to messaging screen

--- src/sections/messages.js
+++ src/sections/messages.js
@@ -43,6 +43,44 @@ function MessageScreen() {
             />
           </button>
         </nav>
+<br></br>
+        <button class="bg-purple-900 hover:bg-purple-700 text-white font-bold py-2 px-4 rounded-full" type="button" data-toggle="collapse" data-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample" >
+                Key Messages
+</button>
+
+<div class="collapse" id="collapseExample">
+<div class="card card-body">
+<div className="messages">
+  <br></br>
+<div class="h-10 w-10 mr-2 text-white bg-yellow-400 rounded-full flex justify-center items-center">
+                            <i class="material-icons"><svg className="h-4 w-4" fill="currentColor" viewBox="0 0 20 20"><path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path></svg></i>
+                        </div>
+          <div>
+            <h2>
+              
+               M2 is due Friday @ 11:59 PM 
+            </h2>
+            
+          </div>
+        </div>
+
+        <div className="messages">
+        <div class="h-10 w-10 mr-2 text-white bg-yellow-400 rounded-full flex justify-center items-center">
+                            <i class="material-icons"><svg className="h-4 w-4" fill="currentColor" viewBox="0 0 20 20"><path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path></svg></i>
+                        </div>
+       
+          <div>
+          
+            <h2>
+              Team 01 Weekly Meeting is Sat @ 10 AM
+            </h2>
+            
+          </div>
+        </div>
+
+</div>
+</div>
+
 
         <div className="messages">
           <img src="https://cdn0.iconfinder.com/data/icons/set-ui-app-android/32/8-512.png"></img>
